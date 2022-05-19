3303a8656e95193a1f4e8eca43ebbe560917e247
Alexander Joossens <alexanderjoossens>
2022-05-19 09:19:26 +0200

    installed docker

--- controller.py
+++ controller.py
@@ -1,7 +1,19 @@
+import docker
+
 def start_controller():
-    print(f'Hi')
+    print(f'Starting controller\n')
+
+    container = client.containers.run('bfirsh/reticulate-splines',
+                                      detach=True)
+    container.logs()
+
+    # to see CPU usage use
+    client.containers.run(cpuset_cpus)
 
 
 if __name__ == '__main__':
+
+    client = docker.from_env()
+    client.containers.run('alpine', 'echo hello world')
     start_controller()
 

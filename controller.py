e78712df3e8e752d85b8236c50dd13bf13bdb872
Alexander Joossens <alexanderjoossens>
2022-05-19 09:27:05 +0200

    controller script

--- controller.py
+++ controller.py
@@ -7,8 +7,18 @@ def start_controller():
                                       detach=True)
     container.logs()
 
+    #create containers
+    client.containers.create()
+
     # to see CPU usage use
-    client.containers.run(cpuset_cpus)
+    # bash command:
+    # docker run --cpuset-cpus="0" -d --rm --name parsec \
+    # anakli/parsec:blackscholes-native-reduced \
+    # ./bin/parsecmgmt -a run -p blackscholes -i native -n 2
+    client.containers.run(cpuset_cpus=0, detach=True, remove=Tre, name="Parsec", threads="2")
+
+    while (cpu usage is smaller than a certain value)
+        ...
 
 
 if __name__ == '__main__':
